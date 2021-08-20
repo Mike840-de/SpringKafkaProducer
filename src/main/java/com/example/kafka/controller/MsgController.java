9db10e3b63754278ee80749de8062a1569e67a7b
mderzhavin <mulkapestri@gmail.com>
2021-08-20 16:31:09 +0300

    commit

--- src/main/java/com/example/kafka/controller/MsgController.java
+++ src/main/java/com/example/kafka/controller/MsgController.java
@@ -1,23 +1,23 @@
 package com.example.kafka.controller;
 
+import com.example.kafka.dto.UserDto;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/msg")
 public class MsgController {
 
     @Autowired
-    private KafkaTemplate<Long, String> kafkaTemplate;
+    private KafkaTemplate<Long, UserDto> kafkaTemplate;
 
     @PostMapping
-    public void sendOrder(String msgId, String msg){
-        ListenableFuture<SendResult<Long, String>> future = kafkaTemplate.send("msg", 1L, msg);
+    public void sendOrder(@RequestParam Long msgId, @RequestBody UserDto userDto){
+        ListenableFuture<SendResult<Long, UserDto>> future = kafkaTemplate.send("msg", msgId, userDto);
         future.addCallback(System.out::println, System.err::println);
         kafkaTemplate.flush();
     }
